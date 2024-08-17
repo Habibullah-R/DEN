dc436800deb3cc718032fefbbf868f072017c6ec
Habibullah-R <habibullahrashid150@gmail.com>
2024-08-17 14:09:50 +0500

    pre installation

--- Blog app/src/app.js
+++ Blog app/src/app.js
@@ -3,6 +3,9 @@ import express, { urlencoded } from "express";
 import cors from "cors"
 import userRoutes from "./routes/user.routes.js"
 import postRoutes from "./routes/post.routes.js"
+import path from "path"
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -12,7 +15,15 @@ app.use(express.static("public"))
 app.use(express.json({limit:"16kb"}));
 app.use(urlencoded({extended:true,limit:"16kb"}))
 
+
 app.use('/api/v1/user',userRoutes)
 app.use('/api/v1/post',postRoutes)
+
+
+app.use(express.static(path.join(__dirname,"/front-end/dist")))
+
+app.use('*',(req,res)=>{
+    res.sendFile(path.join(__dirname , 'front-end','dist','index.html'))
+})
 
 export default app;
